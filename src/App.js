c546367dd234b938aa63fd6ddd76a1922bb37801
Nestor Otondo <oto23001@byui.edu>
2025-02-22 15:49:58 -0300

    Creating base project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Bienvenido Neuron IT Solutions Ltda.</h1>
+      <p>Este es un ejemplo simple usando React sin frameworks adicionales.</p>
+    </div>
+  );
+};
+
+export default App;
