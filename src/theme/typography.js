7ff7fd1685ecc8e253e773893d776753742656fc
Nestor Otondo <notondo@byupathway.edu>
2025-03-31 16:14:07 -0300

    Redefine next JS project

--- src/theme/typography.js
+++
@@ -1,8 +0,0 @@
-const typography = {
-  fontFamily: "'Roboto Condensed'",
-  body1: { fontSize: "1rem", fontWeight: 400 },
-  buttonHome: { fontFamily:"'Inter'", fontSize: "0.90rem", fontWeight: 500 },
-  h6: { fontFamily:"'Inter'", fontSize: "1.5rem", fontWeight: 500 },
-};
-
-export default typography;
